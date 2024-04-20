877699fdbae6d835796bf2d17725edfb0ff09927
KANNA-NAVEEN <kannanaveen122000@gmail.com>
2024-04-20 22:33:14 +0530

    Deleting question service. I will add it again

--- question-service/src/main/java/com/naveen/questionservice/utils/exceptions/QuestionAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.naveen.questionservice.utils.exceptions;
-
-public class QuestionAlreadyExistsException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public QuestionAlreadyExistsException(String message) {
-		super(message);
-	}
-}
